375257ca1cfe178eb0a80c2049d43cc0e01a2858
akhtar hussain <akhtarhussain0788@gmail.com>
2020-10-17 22:00:40 +0500

    Updates

---
+++ precache-manifest.5239ef3d06e1cdc6fb93ea9cd54f5464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaf78b84e198b7ac8534acb83f18931",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c6bcc9b1c4d85843b28b",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5381e8fc30c2c68af348",
+    "url": "/my-app/static/js/2.b4e3e591.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-app/static/js/2.b4e3e591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bcc9b1c4d85843b28b",
+    "url": "/my-app/static/js/main.686e7efa.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
